8ad38ec215193516647c61e3e0e7160b0aef470d
Dormammu <saomuun@mail.ru>
2021-12-30 16:57:33 +0300

    коррекция

---
+++ Lesson 28 - Индексы и диапазоны/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * индексы и диапазоны
+ */
+
+namespace Lesson_28___Индексы_и_диапазоны
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 2, 10, 5 };
+            Console.WriteLine(myArray[^1]); //Поддерживает >= .Net Core 3.0 - выводит первый элемент с конца
+            Console.WriteLine(myArray[1..4]); //Поддерживает >= .Net Core 3.0 - извлекает диапазон элементов
+        }
+    }
+}
