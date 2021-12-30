8e614e17c0cbdb448318419277ccef8e1e8c5b3a
Dormammu <saomuun@mail.ru>
2021-12-30 16:54:56 +0300

    28 урок про индексы и диапазоны на net core 3.0+

---
+++ C-Sharp-курс(addition)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_курс_addition_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
