87561f683bf2fd4a0736a2331aa64d762ef7dd3f
Dormammu <saomuun@mail.ru>
2022-02-27 11:57:43 +0300

    75

---
+++ Lesson 75 - ПОЛИМОРФИЗМ, виртуальные методы, virtual override/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+/*
+ * Полиморфизм
+ * 
+ * виртуальные методы
+ * 
+ * virtual
+ * 
+ * override
+ */
+namespace Lesson_75___ПОЛИМОРФИЗМ__виртуальные_методы__virtual_override_
+{
+    //virtual и override - дают возможность переопределить работу методов в классе наследнике
+    class Motor
+    {
+        //чтобы метод был доступен на уровно наследника вместо private ставим protected
+        protected virtual void StartEngine()
+        {
+            Console.WriteLine("Двигатель запущен!");
+        }
+
+        //Чтобы переопределить метод для новой реализации в наследнике
+        public virtual void Drive()
+        {
+            StartEngine();
+            Console.WriteLine("Я еду!");
+        }
+    }
+
+    class SportMotor : Motor
+    {
+        protected override void StartEngine() => Console.WriteLine("Рон дон дон");
+
+        //переопределение виртуального метода
+        public override void Drive()
+        {
+            StartEngine();
+            Console.WriteLine("Я быстро еду!");
+        }
+    }
+
+    class Stable
+    {
+        public void Drive(Motor motor)
+        {
+            motor.Drive();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stable stable = new Stable();
+            stable.Drive(new SportMotor());
+        }
+    }
+}
