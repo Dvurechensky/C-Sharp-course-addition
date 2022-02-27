2b5316a6e88152db56b907067a974c1c8586e09d
Dormammu <saomuun@mail.ru>
2022-02-27 21:47:06 +0300

    81

---
+++ Lesson 81 - Структуры и классы - отличия, struct vs class/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_81___Структуры_и_классы___отличия__struct_vs_class_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
